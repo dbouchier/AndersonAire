adeba0bf40237072fa1e7244fbd2aa550f13239f
Donald Bouchier <dj@mnsoftwarefactory.com>
2018-03-29 21:34:52 -0500

    test

--- AndersonAire/Global.asax.cs
+++ AndersonAire/Global.asax.cs
@@ -16,6 +16,7 @@ namespace AndersonAire
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //This is a test
         }
     }
 }
